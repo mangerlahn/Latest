56356ce4a9bc87acd62c4d04fbab2186940636b4
Max Langer <maxlanger@me.com>
2017-02-26 22:34:13 +0100

    Add new controller for showing details of the version changes (for later)

---
+++ Latest/MLMUpdateDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MLMUpdateDetailsViewController.swift
+//  Latest
+//
+//  Created by Max Langer on 26.02.17.
+//  Copyright © 2017 Max Langer. All rights reserved.
+//
+
+import Cocoa
+
+class MLMUpdateDetailsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
