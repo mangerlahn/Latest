7d8284200eb78a57c7c5ed41ec4ff39f6ce2812d
Max Langer <maxlanger@me.com>
2017-02-26 22:31:21 +0100

    Add my default class prefix MLM

--- Latest/MLMUpdateListViewController.swift
+++ Latest/MLMUpdateListViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class ViewController: NSViewController, UpdateCheckerDelegate {
+class MLMUpdateListViewController: NSViewController, UpdateCheckerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
