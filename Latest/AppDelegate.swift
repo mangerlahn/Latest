4e9c7383e2b3e92e28c8fe513371ce5c43cc361a
Max Langer <maxlanger@me.com>
2017-12-28 12:19:46 +0100

    A little reordering

--- Latest/AppDelegate.swift
+++ Latest/AppDelegate.swift
@@ -11,8 +11,6 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
