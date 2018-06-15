ec515a06e44a571e074ed46d44b9e60d03f213e3
Max Langer <maxlanger@me.com>
2018-06-15 07:41:08 +0200

    Replace version checking algorithm with Sparkle implementation

--- Latest/Model/Version.swift
+++ Latest/Model/Version.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+
+
 /**
  A Version represents a single version of an app. It contains both the version number and the build number to uniquely
  identify an app (in theory).
@@ -39,8 +41,6 @@ struct Version : Equatable, Comparable {
     static func ==(lhs: Version, rhs: Version) -> Bool {
         let result = self._check(lhs, rhs)
         return result == .equal
-    }
-    
     static func !=(lhs: Version, rhs: Version) -> Bool {
         return !(lhs == rhs)
     }
@@ -53,6 +53,8 @@ struct Version : Equatable, Comparable {
     static func >=(lhs: Version, rhs: Version) -> Bool {
         let result = self._check(lhs, rhs)
         return result == .equal || result == .newer
+    }
+    
     }
     
     static func <(lhs: Version, rhs: Version) -> Bool {
@@ -73,16 +75,14 @@ struct Version : Equatable, Comparable {
         case older, newer, equal, undefined
     }
     
-    /**
-     Performs the actual check. Currently, the build number is preferred for checking, as it is mostly easier to parse
-     Therefore the results are more accurate
-     */
+    /// Performs the actual check. This version checker is adopted by the Sparkle Framework and slightly adapted.
     private static func _check(_ lhs: Version, _ rhs: Version) -> CheckingResult {
         var v1 : String?
         var v2 : String?
         
         if let b1 = lhs.buildNumber, let b2 = rhs.buildNumber {
-            return self._checkBundleVersion(b1, b2)
+            v1 = b1
+            v2 = b2
         } else {
             v1 = lhs.versionNumber
             v2 = rhs.versionNumber
@@ -92,52 +92,144 @@ struct Version : Equatable, Comparable {
             return .undefined
         }
         
-        while c1.count < c2.count {
-            c1.append(0)
-        }
+        c1.cutVersionPrefix()
+        c2.cutVersionPrefix()
         
-        while c1.count > c2.count {
-            c2.append(0)
-        }
-        
-        for index in (0..<c1.count) {
-            if c1[index] > c2[index] {
-                return .newer
-            } else if c1[index] < c2[index] {
-                return .older
+        let count1 = c1.count
+        let count2 = c2.count
+        for i in 0..<min(count1, count2) {
+            let component1 = c1[i]
+            let component2 = c2[i]
+            
+            if component1.type == component2.type {
+                if component1.type == .number {
+                    let value1 = Int(component1.string)!
+                    let value2 = Int(component2.string)!
+                    
+                    if value1 > value2 {
+                        return .newer // Think "1.3" vs "1.2"
+                    } else if value2 > value1 {
+                        return .older // Think "1.2" vs "1.3"
+                    }
+                } else if component1.type == .string {
+                    let result = component1.string.compare(component2.string)
+                    
+                    switch result {
+                    case .orderedAscending:
+                        return .older // Think "1.2A" vs "1.2B"
+                    case .orderedDescending:
+                        return .newer // Think "1.2B" vs "1.2A"
+                    default: ()
+                    }
+                }
+            } else {
+                // Not the same type? Now we have to do some validity checking
+                if component1.type != .string && component2.type == .string {
+                    return .newer // Think "1.2.3" vs "1.2A"
+                } else if component1.type == .string && component2.type != .string {
+                    return .older // Think "1.2A" vs "1.2.2"
+                }
+                
+                // One is a number and the other is a period. The period is invalid
+                if component1.type == .number {
+                    return .newer // Think "1.2.3" vs "1.2.."
+                }
+                
+                return .older // Think "1.2.." vs "1.2.0"
             }
         }
         
-        return .equal
-    }
-    
-    /// A very dumb bundle checking function. It currently only extracts the digits of the build number and compares them
-    private static func _checkBundleVersion(_ b1: String, _ b2: String) -> CheckingResult {
-        let characterSet = CharacterSet.decimalDigits.inverted
-        
-        let d1 = String(String.UnicodeScalarView(b1.unicodeScalars.filter { !characterSet.contains($0) }))
-        let d2 = String(String.UnicodeScalarView(b2.unicodeScalars.filter { !characterSet.contains($0) }))
-        
-        if let i1 = Int(d1), let i2 = Int(d2) {
-            return i1 < i2 ? .older : i1 == i2 ? .equal : .newer
+        // The versions are equal up to the point where they both still have parts
+        // Lets check to see if one is larger than the other
+        if count1 != count2 {
+            let l = count1 > count2
+            let longerComponents = (l ? c1 : c2)[(l ? count2 : count1)...]
+            guard let component = longerComponents.first(where: { $0.type != .separator }) else {
+                return .equal // Think "1.2" vs "1.2."
+            }
+            
+            if component.type == .number, let number = Int(component.string) {
+                if number == 0 {
+                    return .equal // Think "1.2" vs "1.2.0"
+                }
+                
+                return l ? .newer : .older // Think "1.2" vs "1.2.2"
+            }
+            
+            return l ? .older : .newer // Think "1.2" vs "1.2A"
         }
- 
-        return .undefined
+
+        return .equal // Think "1.2" vs "1.2"
     }
 }
 
+fileprivate typealias VersionComponent = (type: CharacterType, string: String)
+
 /// An extension helping the version checking
-private extension String {
+fileprivate extension String {
     
     /**
      Returns the components of an version number.
-     "1.2.5" returns [1, 2, 5] and "v2.1" returns [2, 1]
-     There are still problems with other formats like "2.3.4 x86" that are currently not solved. This would return [2, 3]
+     Components are grouped by Character type, so "12.3" returns [("12", .number), (".", .separator), ("3", .number)]
      */
-    func versionComponents() -> [Int] {
-        let components = self.components(separatedBy: ".")
-        return components.compactMap({ (component) -> Int? in
-            return Int(component.trimmingCharacters(in: CharacterSet.decimalDigits.inverted))
-        })
+    func versionComponents() -> [VersionComponent] {
+        guard let first = self.first else {
+            return []
+        }
+        
+        let components = self.dropFirst().reduce([(CharacterType.for(first)!, String(first))]) { (result: [VersionComponent], character) -> [VersionComponent] in
+            var result = result
+            var component = result.last!
+            let newType = CharacterType.for(character)
+            
+            if newType != component.type || component.type == .separator {
+                result.append((newType!, String(character)))
+            } else {
+                component.string.append(character)
+                result.removeLast()
+                result.append(component)
+            }
+            
+            return result
+        }
+    
+        return components
     }
+}
+
+fileprivate extension Array where Element == VersionComponent {
+
+    /// Removes any leading v of a version number ("v1.2" -> "1.2")
+    mutating func cutVersionPrefix() {
+        guard let component = self.first else { return }
+        
+        if component.string == "v" {
+            self.removeFirst()
+        }
+    }
+    
+}
+
+// Defining the type of a character
+fileprivate enum CharacterType {
+    
+    case separator // Newlines, punctuation..
+    case number // 0..9
+    case string // Everything else
+    
+    /// Returns the type for a given character
+    static func `for`(_ character: Character) -> CharacterType? {
+        guard let scalar = character.unicodeScalars.first else { return nil }
+        
+        if NSCharacterSet.decimalDigits.contains(scalar) {
+            return .number
+        }
+        
+        if NSCharacterSet.whitespacesAndNewlines.contains(scalar) || NSCharacterSet.punctuationCharacters.contains(scalar) {
+            return .separator
+        }
+        
+        return .string
+    }
+    
 }
