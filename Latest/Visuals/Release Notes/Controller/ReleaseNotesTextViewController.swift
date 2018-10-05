4c388414df431c59da5d27d5eabca64cccaf12d2
Max Langer <maxlanger@me.com>
2018-10-05 21:36:04 +0200

    Attempt to improve resize performance using table view

--- Latest/Visuals/Release Notes/Controller/ReleaseNotesTextViewController.swift
+++ Latest/Visuals/Release Notes/Controller/ReleaseNotesTextViewController.swift
@@ -8,25 +8,54 @@
 
 import Cocoa
 
+fileprivate let ReleaseNotesTextParagraphCellIdentifier = NSUserInterfaceItemIdentifier(rawValue: "ReleaseNotesTextParagraphCellIdentifier")
+
 /// The controller displaying the actual release notes
 class ReleaseNotesTextViewController: NSViewController {
 
     /// The view displaying the release notes
     @IBOutlet weak var textField: NSTextField!
     
-    /// Sets the string in the text view
+    /// The table view holding the release notes
+    @IBOutlet weak var tableView: NSTableView!
+    
+    /// The release notes, split up in paragraphs
+    private var paragraphs = [NSAttributedString]()
+    
+    /// Updates the view with the given release notes
     func set(_ string: NSAttributedString) {
-        self.textField.attributedStringValue = self.format(string)
+        // Format the release notes
+        let text = self.format(string)
+        let string = text.string as NSString
+        
+        // Get all paragraph ranges
+        var location = 0
+        var ranges = [NSRange]()
+        repeat {
+            let paragraphRange = string.paragraphRange(for: NSRange(location: location, length: 0))
+            ranges.append(paragraphRange)
+            
+            location = NSMaxRange(paragraphRange)
+        } while (location < text.length)
+        
+        // Get the attributed substrings
+        self.paragraphs = ranges.map { (range) -> NSAttributedString in
+            //  Remove the last character, which is the paragraph delimiter. Leaving it in would result in an empty line in each table cell
+            let paragraphRange = NSRange(location: range.location, length: range.length - 1)
+            return text.attributedSubstring(from: paragraphRange)
+        }
+        
+        // Update the view
+        self.tableView.reloadData()
     }
     
     /// Updates the text views scroll insets
     func updateInsets(with inset: CGFloat) {
-        self.textField.superview?.enclosingScrollView?.contentInsets.top = inset
+        let scrollView = self.tableView.enclosingScrollView
         
-        self.view.layout()
-        
-        let view = self.textField.enclosingScrollView?.documentView
-        view?.scroll(CGPoint(x: 0, y: (view?.bounds.size.height ?? 0.0) + inset))
+        scrollView?.automaticallyAdjustsContentInsets = false
+        scrollView?.contentInsets.top = inset
+        scrollView?.documentView?.scroll(CGPoint(x: 0, y: -inset))
     }
     
     // MARK: - Private Methods
@@ -44,6 +73,7 @@ class ReleaseNotesTextViewController: NSViewController {
         
         /// Remove all color
         string.removeAttribute(.foregroundColor, range: fullRange)
+        string.addAttribute(.foregroundColor, value: NSColor.labelColor, range: fullRange)
         
         /// Reset font
         string.removeAttribute(.font, range: fullRange)
@@ -60,6 +90,30 @@ class ReleaseNotesTextViewController: NSViewController {
         }
         
         return string
+    }
+    
+}
+
+extension ReleaseNotesTextViewController: NSTableViewDataSource {
+
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.paragraphs.count
+    }
+    
+}
+
+extension ReleaseNotesTextViewController: NSTableViewDelegate {
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.makeView(withIdentifier: ReleaseNotesTextParagraphCellIdentifier, owner: self) as? NSTableCellView
+        
+        cell?.textField?.attributedStringValue = self.paragraphs[row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldTrackCell cell: NSCell, for tableColumn: NSTableColumn?, row: Int) -> Bool {
+        return true
     }
     
 }
