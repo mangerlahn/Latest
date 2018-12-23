0064eb89c5d9f68270c898de4c0fcbfecec81314
Max Langer <maxlanger@me.com>
2018-12-23 23:18:49 +0100

    #38 Remove text background color in release notes

--- Latest/Interface/Release Notes/Controller/ReleaseNotesTextViewController.swift
+++ Latest/Interface/Release Notes/Controller/ReleaseNotesTextViewController.swift
@@ -38,26 +38,29 @@ class ReleaseNotesTextViewController: NSViewController {
     // MARK: - Private Methods
     
     /**
-     This method manipulates the release notes to make them look uniform.
+     This method modifies the release notes to make them look uniform.
      All custom fonts and font sizes are removed for a more unified look. Specific styles like bold or italic parts as well as links are preserved.
      - parameter attributedString: The string to be formatted
      - returns: The formatted string
      */
     private func format(_ attributedString: NSAttributedString) -> NSAttributedString {
         let string = NSMutableAttributedString(attributedString: attributedString)
-        let fullRange = NSMakeRange(0, attributedString.length)
+        let textRange = NSMakeRange(0, attributedString.length)
         let defaultFont = NSFont.systemFont(ofSize: NSFont.systemFontSize)
         
-        /// Remove all color
-        string.removeAttribute(.foregroundColor, range: fullRange)
-        string.addAttribute(.foregroundColor, value: NSColor.labelColor, range: fullRange)
+        // Fix foreground color
+        string.removeAttribute(.foregroundColor, range: textRange)
+        string.addAttribute(.foregroundColor, value: NSColor.labelColor, range: textRange)
+		
+		// Remove background color
+		string.removeAttribute(.backgroundColor, range: textRange)
         
-        /// Reset font
-        string.removeAttribute(.font, range: fullRange)
-        string.addAttribute(.font, value: defaultFont, range: fullRange)
+        // Reset font
+        string.removeAttribute(.font, range: textRange)
+        string.addAttribute(.font, value: defaultFont, range: textRange)
         
         // Copy traits like italic and bold
-        attributedString.enumerateAttribute(NSAttributedString.Key.font, in: fullRange, options: .reverse) { (fontObject, range, stopPointer) in
+        attributedString.enumerateAttribute(NSAttributedString.Key.font, in: textRange, options: .reverse) { (fontObject, range, stopPointer) in
             guard let font = fontObject as? NSFont else { return }
             
             let traits = font.fontDescriptor.symbolicTraits
