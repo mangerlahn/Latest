2a254f6a5d854d50651300bb0ab382f346e84138
Max Langer <maxlanger@me.com>
2019-07-13 09:52:22 +0200

    Add mas-cli as submodule

--- Frameworks/CommerceKit/CKDownloadDirectory.h
+++
@@ -1,9 +0,0 @@
-//
-//  CKDownloadDirectory.h
-//  mas-cli
-//
-//  Created by Ben Chatelain on 7/8/18.
-//  Copyright © 2018 mas-cli. All rights reserved.
-//
-
-NSString* CKDownloadDirectory(NSString *target);
