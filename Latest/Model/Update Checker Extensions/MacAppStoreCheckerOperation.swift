f0bb243be12c620cfbc210e111b245a580d6c82c
Max Langer <maxlanger@me.com>
2021-12-26 12:02:41 +0100

    #106 Fix Latest reporting updates for iPad software

--- Latest/Model/Update Checker Extensions/MacAppStoreCheckerOperation.swift
+++ Latest/Model/Update Checker Extensions/MacAppStoreCheckerOperation.swift
@@ -25,9 +25,6 @@ class MacAppStoreUpdateCheckerOperation: StatefulOperation, UpdateCheckerOperati
 	
 	/// The bundle to be checked for updates.
 	private let app: MacAppStoreAppBundle
-	
-	/// The url to check for updates.
-	private let url: URL
 		
 	required init?(withAppURL appURL: URL, version: String, buildNumber: String, completionBlock: @escaping UpdateCheckerCompletionBlock) {
         let appName = appURL.lastPathComponent as NSString
@@ -40,14 +37,9 @@ class MacAppStoreUpdateCheckerOperation: StatefulOperation, UpdateCheckerOperati
 			  fileManager.fileExists(atPath: receiptPath) || receiptPath.contains("WrappedBundle") else { return nil }
         
         // App is from Mac App Store
-        let languageCode = Locale.current.regionCode ?? "US"
+        guard let bundleIdentifier = appBundle?.bundleIdentifier else { return nil }
 
-        guard let bundleIdentifier = appBundle?.bundleIdentifier,
-              let url = URL(string: "https://itunes.apple.com/lookup?bundleId=\(bundleIdentifier)&country=\(languageCode)&entity=macSoftware&limit=1")
-              else { return nil }
-
-		// The requirements are met, the url points to a Mac App Store app. Initialize the operation.
-		self.url = url
+		// The requirements are met. Initialize the operation.
 		self.app = MacAppStoreAppBundle(appName: appName.deletingPathExtension, bundleIdentifier: bundleIdentifier, versionNumber: version, buildNumber: buildNumber, url: appURL)
 		
 		super.init()
@@ -63,24 +55,74 @@ class MacAppStoreUpdateCheckerOperation: StatefulOperation, UpdateCheckerOperati
             return
         }
         
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
-        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard error == nil,
-                let data = data,
-                let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                let results = json["results"] as? [Any],
-                results.count != 0,
-                let appData = results[0] as? [String: Any] else {
-					self.app.newestVersion.releaseNotes = error
-					self.finish()
-                    return
-            }
-            
-			self.parse(data: appData)
-        }
-        
-        dataTask.resume()
+		self.fetchAppInfo { data, error in
+			// Process fetched info
+			if let data = data {
+				self.parse(data: data)
+			}
+			
+			// Forward fetch error
+			else {
+				self.app.newestVersion.releaseNotes = error
+				self.finish()
+			}
+		}
+	}
+	
+	private func fetchAppInfo(completion: @escaping (_ data: [String : Any]?, _ error: Error?) -> ()) {
+		// We need a two-level fetch process. `desktopSoftware` delivers metadata for mac-native software. `macSoftware` seems to be more broad, also includes Catalyst and iOS-only software. The former however is more accurate, as `macSoftware` might return iPad metadata for certain apps. We therefore prefer `desktopSoftware` and fall back to `macSoftware` if no info was found.
+		self.fetchAppInfo(with: "desktopSoftware") { data, error in
+			// Success, forward data
+			if let data = data {
+				completion(data, error)
+				return
+			}
+			
+			// Data could not be fetched, try the broader entity type
+			self.fetchAppInfo(with: "macSoftware") { data, error in
+				completion(data, error)
+			}
+		}
+	}
+	
+	private func fetchAppInfo(with entityType: String, completion: @escaping (_ data: [String : Any]?, _ error: Error?) -> ()) {
+		// Build URL
+		guard let endpoint = URL(string: "https://itunes.apple.com/lookup") else {
+			completion(nil, nil)
+			return
+		}
 
+		// Add parameters
+		let languageCode = Locale.current.regionCode ?? "US"
+		var components = URLComponents(url: endpoint, resolvingAgainstBaseURL: false)
+		components?.queryItems = [
+			URLQueryItem(name: "limit", value: "1"),
+			URLQueryItem(name: "entity", value: entityType),
+			URLQueryItem(name: "country", value: languageCode),
+			URLQueryItem(name: "bundleId", value: self.app.bundleIdentifier)
+		]
+		guard let url = components?.url else {
+			completion(nil, nil)
+			return
+		}
+		
+		// Perform request
+		let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30)
+		let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+			guard error == nil,
+				let data = data,
+				let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+				let results = json["results"] as? [Any],
+				results.count != 0,
+				let appData = results[0] as? [String: Any] else {
+					completion(nil, error)
+					return
+			}
+			
+			completion(appData, error)
+		}
+		
+		dataTask.resume()
 	}
 	
 }
