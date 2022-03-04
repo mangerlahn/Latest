975121cdc8de7c30990c15ddda6d9b18bbec5054
Max Langer <maxlanger@me.com>
2022-03-04 23:47:47 +0100

    Remove Mac App Store in-app update mechanism

--- Latest/Model/App Bundles/MacAppStoreAppBundle.swift
+++ Latest/Model/App Bundles/MacAppStoreAppBundle.swift
@@ -7,7 +7,6 @@
 //
 
 import Cocoa
-import CommerceKit
 
 /**
  Mac App Store app bundle subclass, it handles the parsing of the iTunes JSON
@@ -16,11 +15,6 @@ class MacAppStoreAppBundle: AppBundle {
 
     /// The url of the app in the Mac App Store
     var appStoreURL : URL?
-	
-	/// The update information provided by CommerceKit, if available
-	var updateInformation: CKUpdate? {
-		return CKUpdateController.shared()?.availableUpdates().first(where: { $0.bundleID == self.bundleIdentifier })
-	}
 	
 	/// The icon representing the source of the app.
 	override class var sourceIcon: NSImage? {
@@ -38,7 +32,6 @@ class MacAppStoreAppBundle: AppBundle {
 	override func update() {
 		// This is a temporary workaround to disable direct updates of Mac App Store apps, which does not work anymore.
 		NSWorkspace.shared.open(self.appStoreURL ?? self.url)
-		// UpdateQueue.shared.addOperation(MacAppStoreUpdateOperation(app: self))
 	}
 	
 }
